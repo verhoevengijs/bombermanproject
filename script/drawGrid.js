420bd3f7d73554ff266da915a95b523374986a96
stefan <stefan.de.graaf.013@hotmail.com>
2018-02-09 10:48:52 +0100

    hue

--- script/drawGrid.js
+++ script/drawGrid.js
@@ -1,24 +1,3 @@
-// const canvas = document.getElementById('canvas');
-//
-// const drawGrid = function (h,w) {
-//     const ctx = canvas.getContext('2d');
-//     ctx.canvas.width = w;
-//     ctx.canvas.height = h;
-//
-//     for (x=0;x<=w;x+=50) {
-//         for (y=0;y<=h;y+=50) {
-//             ctx.moveTo(x, 0);
-//             ctx.lineTo(x, h);
-//             ctx.stroke();
-//             ctx.moveTo(0, y);
-//             ctx.lineTo(w, y);
-//             ctx.stroke();
-//         }
-//     }
-// };
-// drawGrid(650, 750);
-
-
 const drawGrid = function (height, width) {
     const canvas = document.getElementById('canvas');
 
