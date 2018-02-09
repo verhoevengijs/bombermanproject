e27e2d06bbc9256c5f58b9e85390697d59360c54
Paco Lee <pacootjelee3110@gmail.com>
2018-02-09 12:10:51 +0100

    First push

--- script/drawGrid.js
+++ script/drawGrid.js
@@ -18,20 +18,21 @@
 // };
 // drawGrid(650, 750);
 
-
 const drawGrid = function (height, width) {
     const canvas = document.getElementById('canvas');
-
     const ctx = canvas.getContext('2d');
     ctx.canvas.width = width;
     ctx.canvas.height = height;
-    for (let x=0;x<=width;x+=50){
-        for (let y=0;y<=height;y+=50) {
-            ctx.fillStyle = 'rgb(' + Math.floor(255 - 42.5 * y) + ', ' +
-                Math.floor(255 - 42.5 * x) + ', 0)';
-            ctx.fillRect(x, y, 50, 50);
+    const wall = new Image();
+    wall.addEventListener('load', function() {
+        for (let x=0;x<=width;x+=50){
+            for (let y=0;y<=height;y+=50) {
+                ctx.drawImage(wall,x, y, 50, 50);
+                // ctx.strokeRect(x, y, 50, 50);
+            }
         }
-    }
-};
+    });
+    wall.src = 'tile_wall.png';
 
+};
 drawGrid(650,750);
