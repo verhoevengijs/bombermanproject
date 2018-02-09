ce1f36a751b5b01a06598864184909b45cfd471e
Paco Lee <pacootjelee3110@gmail.com>
2018-02-09 09:57:35 +0100

    First push

--- script/drawGrid.js
+++ script/drawGrid.js
@@ -1,12 +1,12 @@
-var drawGrid = function(w, h, id) {
-    var canvas = document.getElementById(id);
-    var ctx = canvas.getContext('2d');
-    ctx.canvas.width  = w;
+const canvas = document.getElementById('canvas');
+
+const drawGrid = function (h,w) {
+    const ctx = canvas.getContext('2d');
+    ctx.canvas.width = w;
     ctx.canvas.height = h;
 
-
-    for (x=0;x<=w;x+=20) {
-        for (y=0;y<=h;y+=20) {
+    for (x=0;x<=w;x+=50) {
+        for (y=0;y<=h;y+=50) {
             ctx.moveTo(x, 0);
             ctx.lineTo(x, h);
             ctx.stroke();
@@ -15,8 +15,5 @@ var drawGrid = function(w, h, id) {
             ctx.stroke();
         }
     }
-
 };
-
-
-drawGrid(800, 400, "grid");
+drawGrid(650, 750);
