77aab90af77ecca56db70d5352cd0dda3aef0b11
Paco Lee <pacootjelee3110@gmail.com>
2018-02-28 10:25:21 +0100

    draw muur

--- script/drawGrid.js
+++ script/drawGrid.js
@@ -8,21 +8,26 @@ const drawArena = function (height, width) {
     wall.addEventListener('load', function() {
         const grid = [
             [{k:'muur'},{k:'muur'},{k:'muur'},{k:'muur'},{k:'muur'},{k:'muur'},{k:'muur'},{k:'muur'},{k:'muur'},{k:'muur'},{k:'muur'},{k:'muur'},{k:'muur'}],
-            [{k:'muur'},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''}],
-            [{k:'muur'},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''}],
-            [{k:'muur'},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''}],
-            [{k:'muur'},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''}],
-            [{k:'muur'},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''}],
-
+            [{k:'muur'},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:'muur'}],
+            [{k:'muur'},{k:''},{k:'muur'},{k:''},{k:'muur'},{k:''},{k:'muur'},{k:''},{k:'muur'},{k:''},{k:'muur'},{k:''},{k:'muur'},{k:''},{k:'muur'},{k:'muur'}],
+            [{k:'muur'},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:'muur'}],
+            [{k:'muur'},{k:''},{k:'muur'},{k:''},{k:'muur'},{k:''},{k:'muur'},{k:''},{k:'muur'},{k:''},{k:'muur'},{k:''},{k:'muur'},{k:''},{k:''},{k:'muur'}],
+            [{k:'muur'},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:'muur'}],
+            [{k:'muur'},{k:''},{k:'muur'},{k:''},{k:'muur'},{k:''},{k:'muur'},{k:''},{k:'muur'},{k:''},{k:'muur'},{k:''},{k:'muur'},{k:''},{k:''},{k:'muur'}],
+            [{k:'muur'},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:'muur'}],
+            [{k:'muur'},{k:''},{k:'muur'},{k:''},{k:'muur'},{k:''},{k:'muur'},{k:''},{k:'muur'},{k:''},{k:'muur'},{k:''},{k:'muur'},{k:''},{k:''},{k:'muur'}],
+            [{k:'muur'},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:'muur'}],
+            [{k:'muur'},{k:''},{k:'muur'},{k:''},{k:'muur'},{k:''},{k:'muur'},{k:''},{k:'muur'},{k:''},{k:'muur'},{k:''},{k:'muur'},{k:''},{k:''},{k:'muur'}],
+            [{k:'muur'},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:'muur'}],
+            [{k:'muur'},{k:''},{k:'muur'},{k:''},{k:'muur'},{k:''},{k:'muur'},{k:''},{k:'muur'},{k:''},{k:'muur'},{k:''},{k:'muur'},{k:''},{k:''},{k:'muur'}],
+            [{k:'muur'},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:''},{k:'muur'}],
+            [{k:'muur'},{k:'muur'},{k:'muur'},{k:'muur'},{k:'muur'},{k:'muur'},{k:'muur'},{k:'muur'},{k:'muur'},{k:'muur'},{k:'muur'},{k:'muur'},{k:'muur'}],
         ];
 
         grid.forEach(function (rows, rowindex) {
-            grid.forEach(function (obj, colindex) {
-                if (obj.k === 'b') {
-                    const b = new Bomb();
-                }
+            rows.forEach(function (obj, colindex) {
                 if (obj.k === 'muur') {
-                    const muur = new Muur();
+                    new Muur(ctx);
                 }
                 ctx.strokeRect(rowindex * 50, colindex * 50, 50 ,50);
                 console.log(rowindex + ' - ' + colindex)
---
+++ script/muur.js
@@ -0,0 +1,5 @@
+function Muur(ctx) {
+    let img = new Image();
+    img.src = '../images/tile_wall.png';
+    ctx.drawImage(img,50,50);
+}
